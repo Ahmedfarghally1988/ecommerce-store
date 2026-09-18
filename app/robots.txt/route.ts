ef386c751abd75aa5f42efedb610352cd6df4eda
Ahmed Ahmed Farghally <34854286+Ahmedfarghally1988@users.noreply.github.com>
2026-09-19 01:59:39 +0300

    last_v

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const setting = await prisma.setting.findUnique({
     where: { key: 'robots_txt' }
